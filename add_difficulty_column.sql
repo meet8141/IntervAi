1e6088691e9e12860b08894dd51070085244aa73
meet8141 <meetpatel814116@gmail.com>
2026-09-18 15:49:10 +0000

    Refactor the folder struture

--- add_difficulty_column.sql
+++
@@ -1,8 +0,0 @@
--- Add difficulty column to mockinterview table
-ALTER TABLE mockinterview 
-ADD COLUMN difficulty VARCHAR(50);
-
--- Set default value for existing records
-UPDATE mockinterview 
-SET difficulty = 'Intermediate' 
-WHERE difficulty IS NULL;
